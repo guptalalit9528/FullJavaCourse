89161564ec40bb1200c3800f63b719a46d845f11
Lalit <lalitgupta1qq@gmail.com>
2025-12-31 19:03:09 +0530

    Unnary operator

---
+++ src/javabasics/UnaryOp.java
@@ -0,0 +1,12 @@
+package javabasics;
+
+public class UnaryOp {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("This is a PreIncrement operator : " +(++a));
+        System.out.println("This is a PostIncrement operator : " +(a++));
+        System.out.println("This is a PreDecriment operator : " +(--a));
+        System.out.println("This is a PostDecriment operator : " +(a--));
+
+    }
+}
