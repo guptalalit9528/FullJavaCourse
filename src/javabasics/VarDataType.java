d3ef1836150a328fb654111892c7a2f91dca3867
Lalit <lalitgupta1qq@gmail.com>
2025-12-31 19:03:24 +0530

    variable datatype

---
+++ src/javabasics/VarDataType.java
@@ -0,0 +1,9 @@
+package javabasics;
+
+public class VarDataType {
+    public static void main(String[] args) {
+        int a = 10;//(int :- Datatype), (a :- variable name), (10 :- this is a Data)
+        System.out.println("this is a define variable and datatype and value");
+        System.out.println("print the value :-" +a);
+    }
+}
