0dca4bed5a6ca336285c83fd7a2a1fa87f180700
Lalit <lalitgupta1qq@gmail.com>
2025-12-31 19:02:07 +0530

    Airthmethic operator

---
+++ src/javabasics/AirthmeticOp.java
@@ -0,0 +1,14 @@
+package javabasics;
+
+public class AirthmeticOp {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        System.out.println("This is a Addition operator : " +(a+b));
+        System.out.println("This is a Subtraction operator : " +(a-b));
+        System.out.println("This is a Multiplication operator : " +(a*b));
+        System.out.println("This is a Division operator : " +(a/b));
+        System.out.println("This is a Modulas operator : " +(a%b));
+
+    }
+}
