4e845465d6a4c24b31782609ac2327ff22494a12
Lalit <lalitgupta1qq@gmail.com>
2026-01-02 23:08:21 +0530

    Assignment operator

---
+++ src/javabasics/AssignmentOperator.java
@@ -0,0 +1,13 @@
+package javabasics;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = a;
+        System.out.println(b);
+        System.out.println(a+=20);
+        System.out.println(a-=30);
+        System.out.println(a/=5);
+        System.out.println(a*=10);
+    }
+}
