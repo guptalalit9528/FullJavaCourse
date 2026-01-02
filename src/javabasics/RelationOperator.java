34e5d8279875995edecca21580f71882d517b89f
Lalit <lalitgupta1qq@gmail.com>
2026-01-02 23:08:56 +0530

    Relational operator

---
+++ src/javabasics/RelationOperator.java
@@ -0,0 +1,14 @@
+package javabasics;
+
+public class RelationOperator {
+    public static void main(String[] args) {
+        int i = 20;
+        int j = 30;
+        System.out.println("Equal to : "+(i==j));
+        System.out.println("Greater than : " +(i>j));
+        System.out.println("Less then : " +(i<j));
+        System.out.println("grater than equal to : "+(i>=j));
+        System.out.println("Less then equal to : "+(i<=j));
+        System.out.println("Not equal to : "+(i!=j));
+    }
+}
