177ce5c06a9575dd4feb303b904da4ec45b8764a
Lalit <lalitgupta1qq@gmail.com>
2026-01-02 23:09:14 +0530

    Unnary operator

---
+++ src/javabasics/UnarryOperator.java
@@ -0,0 +1,12 @@
+package javabasics;
+
+public class UnarryOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("PreIncrement : "+(++a));
+        System.out.println("PostIncrement : "+(a++));
+        System.out.println("PreDecrement : "+(--a));
+        System.out.println("PostDecrement : "+(a--));
+        System.out.println("only print statement : "+a);
+    }
+}
